4a679651e9d6f4bd578d094ed7a4dda86fcb15d4
lydia <lydia_andrews18@yahoo.com>
2018-03-18 20:16:40 -0400

    updated the UI to have more validations

--- target/classes/static/js/dataRetrive.js
+++ target/classes/static/js/dataRetrive.js
@@ -1,4 +1,4 @@
-angular.module("dataRetrive", ['ngAnimate', 'ui.bootstrap'])
+angular.module("dataRetrive", ['ngAnimate', 'ui.bootstrap','ui.grid'])
 	.controller('dataController', function($scope, $http) {
      $http.get("http://localhost:8888/employeeinfo/")
                     .success(function (data) {
@@ -9,34 +9,36 @@ angular.module("dataRetrive", ['ngAnimate', 'ui.bootstrap'])
                     	  $scope.orderByField = 'salary';
                     	  $scope.reverseSort = false;
                           $scope.employees=data;
-                          console.log($scope.employees);
                           $scope.IsVisible = false;
+                          $scope.errorIsVisible = false;
            })
  
            $scope.employeeDetails = function(){
     	    $scope.empName = 'Employee ' + $scope.selectedEmployee;
-    	    console.log("employee details");
     	  }
      	  $scope.getEmployeDetails = function(searchName){
-     		 $scope.IsVisible = $scope.IsVisible ? false : true;
-     	     console.log($scope.IsVisible);
+     		 $scope.errorIsVisible = false;
+     		 $scope.IsVisible = false;
      	    $scope.empsel="";
      	    for (var i = 0, len = $scope.employees.length; i < len; i += 1) {
-     	    	console.log(i);
      	        if ($scope.employees[i].name === searchName) {
      	        	 $scope.empsel=$scope.employees[i];
-     	        	 console.log($scope.empsel);
-     	        	 i=$scope.employees.length
+     	        	 i=$scope.employees.length;
      	        }
      	    
      	       
      	    }
      	   $http.get("http://localhost:8888/employeedetail?id="+$scope.empsel.id)
-           .success(function (detail) {
-        	 $scope.empDetails=detail
-        	 console.log($scope.empDetails);
+           .then(function (detail) {
+        	 console.log(detail.status);
+        	 $scope.empDetails=detail.data;
+        	 $scope.IsVisible = true;
+     			  },function(errRes){
+     				 $scope.IsVisible = false;
+     				 $scope.errorIsVisible = true;
+     				 $scope.errorMessage = 'Request Failed';
      			  })
-     	  }
+     			  }
             });
 
             
